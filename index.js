0e8223c1ed476721aafc2355d5acb832e4ce217c
Fazekas Adrienn <adreeja17@gmail.com>
2022-11-30 23:13:32 +0100

    create buttons and display messages

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 3
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    let hasBlackjack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " , " + secondCard
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        let hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-console.log(message)
+
+function newCard() {
+    console.log("clicked")
+}
